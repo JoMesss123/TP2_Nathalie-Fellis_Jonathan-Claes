a41a74d1aa8064f7a49ea029eb60245d07331834
TalieF <taliefellis@gmail.com>
2023-06-19 06:41:16 -0400

    Ajout partial Variables

---
+++ script.js
@@ -0,0 +1,17 @@
+// MENU MOBILE
+const btn = document.getElementById('menu_btn');
+const nav = document.getElementById('menu');
+const barsIcon = document.querySelector('.fa-bars-staggered');
+const timesIcon = document.querySelector('.fa-times');
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('open');
+    nav.classList.toggle('hidden');
+    document.body.classList.toggle('overflow-hidden');
+    toggleMenuIcon();
+});
+
+function toggleMenuIcon() {
+    barsIcon.classList.toggle('hidden');
+    timesIcon.classList.toggle('hidden');
+}
